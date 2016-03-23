7781323cdd7fbe66c3fee431c586aa8be5a8f79c
Matthias Shapiro <matthias.shapiro@gmail.com>
2016-03-23 16:56:40 -0700

    Commit for Module 1 begin, switching to the master branch

--- Workshop/Module1-AdaptiveUI/Source/Begin/Microsoft.Labs.SightsToSee/Microsoft.Labs.SightsToSee.Library/Models/Restaurant.cs
+++ Workshop/Module1-AdaptiveUI/Source/Begin/Microsoft.Labs.SightsToSee/Microsoft.Labs.SightsToSee.Library/Models/Restaurant.cs
@@ -254,5 +254,12 @@ namespace Microsoft.Labs.SightsToSee.Library.Models
         {
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
+
+        
+    }
+    public class EatsGroup
+    {
+        public string GroupName { get; set; }
+        public List<Restaurant> ListOfEats { get; set; }
     }
 }
