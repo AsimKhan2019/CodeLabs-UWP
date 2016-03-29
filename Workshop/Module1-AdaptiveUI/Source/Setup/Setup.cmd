70a40c8adbc1cb7ae9c0ecc9881d3d834f545c48
Andy Wigley <andy.wigley@outlook.com>
2016-03-28 17:51:42 -0700

    Corrected

--- Workshop/Module1-AdaptiveUI/Source/Setup/Setup.cmd
+++ Workshop/Module1-AdaptiveUI/Source/Setup/Setup.cmd
@@ -6,6 +6,8 @@ ECHO Install Visual Studio 2015 Code Snippets for the module:
 ECHO -------------------------------------------------------------------------------
 CALL .\Scripts\InstallCodeSnippets.cmd
 ECHO Done!
+ECHO.
+CD "%~dp0"
 ECHO Create desktop shortcut for the workshop folders:
 ECHO -------------------------------------------------------------------------------
 CALL .\Scripts\CreateShortcut.cmd
