fd9e3624deb36f62f68149cc577ce2b922f8c742
Andy Wigley <andy.wigley@outlook.com>
2016-03-29 20:02:47 -0700

    ARM appxpackage

---
+++ Workshop/Module1-AdaptiveUI/Source/End/AppxPackage/deploysights2see.cmd
@@ -0,0 +1,1 @@
+%ProgramFiles(x86)%\Windows Kits\10\bin\x86>WinAppDeployCmd install -file "c:\labs\CodeLabs-UWP\Workshop\Module1-AdaptiveUI\Source\end\appxpackage\Microsoft.Labs.SightsToSee_1.1.0.0_arm_Debug.appxbundle" -ip 127.0.0.1
